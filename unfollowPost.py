c3b277826c52a155e4405f8850aaf87daccb4ae5
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2018-01-15 22:02:27 +0530

    reading json for for ids and user login data

--- unfollowPost.py
+++ unfollowPost.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import json
 
 driver = webdriver.Firefox()
 driver.get("https://www.quora.com/")
+
+ids = json.load(open('fieldIds.json'))
+userData = json.load(open('userData.json'))
+
+emailId = ids["loginPage"]["email"]
+passwordId = ids["loginPage"]["password"]
+
+username = userData["login"]["username"]
+password = userData["login"]["password"]
+
+# print(emailId, passwordId, username, password)
