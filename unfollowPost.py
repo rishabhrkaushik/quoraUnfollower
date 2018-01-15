e87336dbc651c1a4f7844b2610cc45d9b1b78933
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2018-01-15 23:08:24 +0530

    login function completed and fieldIds removed

--- unfollowPost.py
+++ unfollowPost.py
@@ -1,17 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import json
+import time
 
 driver = webdriver.Firefox()
 driver.get("https://www.quora.com/")
 
-ids = json.load(open('fieldIds.json'))
 userData = json.load(open('userData.json'))
-
-emailId = ids["loginPage"]["email"]
-passwordId = ids["loginPage"]["password"]
 
 username = userData["login"]["username"]
 password = userData["login"]["password"]
 
-# print(emailId, passwordId, username, password)
+# print( username, password)
+Xpath="//input[contains(@id,'_email')]"	
+emailField = driver.find_elements_by_xpath(Xpath)
+emailField[1].send_keys(username)
+
+Xpath="//input[contains(@id,'_password')]"	
+passwordField = driver.find_elements_by_xpath(Xpath)
+passwordField[1].send_keys(password)
+time.sleep(5)
+passwordField[1].send_keys(Keys.ENTER)
+time.sleep(5)
+
+print("Login activity complete")
